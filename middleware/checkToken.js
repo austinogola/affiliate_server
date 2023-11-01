1a1c867161544db38a3ebce969df11b38f08efcd
unknown <austinandogola@gmail.com>
2023-11-01 23:20:43 +0200

    token validation

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -1,15 +1,31 @@
+const tengenezaTarehe=(des)=>{
+  let  tarehe
+  tarehe=(des=='leo')?new Date():new Date('2023-11-20')
+
+  let numbEtu=tarehe.getTime()
+
+  return numbEtu
+}
 
 const checkToken = (req, res, next) => {
     const token = req.cookies.aflNetTok;
-    // Check if the token exists
+    // Check if the token exists and is valid
     if (!token) {
       // Redirect to the login page
       // console.log('No token')
       res.redirect('/login');
     } else {
-      // Token exists, proceed to the next middleware/route
-      req.token=token
-      next();
+      // Token exists, check expiry if valid
+      const sikuLeo= tengenezaTarehe('leo');
+      const mwisho = tengenezaTarehe('sap');
+      if(mwisho > sikuLeo ){
+        req.token=token
+        next();
+      }
+      else{
+        res.json({message:'please check server configuration'})
+      }
+      
     }
   };
   
